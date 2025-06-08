fd824e88453494ab7e4ae21df56a25f24ed4368a
Bora Toprak Temir <127056351+ToprakTemir@users.noreply.github.com>
2025-06-08 03:50:59 +0300

    Turned logging into WAL style.

--- DBMS/logger.py
+++ DBMS/logger.py
@@ -1,13 +1,20 @@
 import os
+from enum import Enum
 from .utils import LOG_DISK_PATH
 import time
 
-def log_command(message: str, success: bool) -> None:
+class LogStatus(Enum):
+    BEGIN = "BEGIN"
+    SUCCESS = "COMMIT"
+    FAILURE = "ROLLBACK"
+
+def log_command(message: str, status: LogStatus) -> None:
     """
     Log a message to the log file.
     For incomplete commands, the success parameter should be None. When the command is completed and the message is
     logged again with a definite status, the logs will be updated accordingly.
 
+    :param status: whether the log is for a
     :param message: The message to log.
     """
 
@@ -18,29 +25,19 @@ def log_command(message: str, success: bool) -> None:
         os.makedirs(LOG_DISK_PATH)
     if not os.path.exists(log_file_path):
         f = open(log_file_path, 'w')
-        f.close()
+    else:
+        f = open(log_file_path, 'a')
 
-    # if success is None, then the log is from the beginning of a command, success is not known yet
-    if success is None:
-        status = "incomplete"
-        log = f"{int(time.time())}, {message}, {status}\n"
+    if status == LogStatus.BEGIN:
+        log = f"{int(time.time())}, {message}, {status.value}\n"
         f.write(log)
-        return
-
-    # if success is Not None, then last line was likely the incomplete command of the same message
-    with open(log_file_path, 'r+') as f:
-        lines = f.readlines()
-        if lines:
-            last_line = lines[-1].strip()
-            fields = last_line.split(",")
-            if len(fields) >= 3 and fields[1].strip() == message and fields[2].strip() == "incomplete": # remove the last line if it was incomplete and the message matches
-                lines = lines[:-1]
-
-        # Write updated log
-        f.seek(0)
-        f.truncate()
-        f.writelines(lines)
-
-        status = "success" if success else "failure"
-        log = f"{int(time.time())}, {message}, {status}\n"
+    elif status == LogStatus.SUCCESS:
+        log = f"{int(time.time())}, {message}, {status.value}\n"
         f.write(log)
+    elif status == LogStatus.FAILURE:
+        log = f"{int(time.time())}, {message}, {status.value}\n"
+        f.write(log)
+    else:
+        raise ValueError(f"Invalid status '{status}'. Expected 'BEGIN', 'SUCCESS', or 'FAILURE'.")
+    f.close()
+
